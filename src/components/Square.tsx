133cd58409d701c853879eb486563843fb6e3f95
Thiago <tlcrezende@gmail.com>
2023-02-23 17:20:14 -0300

    add square component

---
+++ src/components/Square.tsx
@@ -0,0 +1,9 @@
+import "../styles.css";
+
+export default function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
