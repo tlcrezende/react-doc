5b5e3cea09e1a39e5fcb6a05615e2ab3a022276f
Thiago <tlcrezende@gmail.com>
2023-02-23 17:19:53 -0300

    add style

--- src/assets/components/MyButton.tsx
+++
@@ -1,8 +0,0 @@
-function MyButton({count, onClick}:{count: number, onClick: any}): any {
-
-  return (
-    <button className="square" onClick={onClick}>Click me and this will change {count}</button>
-  );
-}
-
-export default MyButton;
